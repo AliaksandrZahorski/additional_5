0f511f3967c6ce49d6d74b5d90e2b82ea6c6b8cb
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2017-12-11 16:46:50 +0300

    solution has been added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = new Map();
+  bracketsConfig.map(b => brackets.set(b[0], b[1]));
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++){
+    const c = str.charAt(i);
+    if (brackets.has(c)) {
+      if (c === brackets.get(c) && stack.length > 0) {
+        const t = stack.pop();
+        if (t !== c) {
+          stack.push(t);
+          stack.push(c);
+        }
+      } else {
+        stack.push(c);
+      }
+    } else {
+      if (stack.length === 0 || c !== brackets.get(stack.pop())) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0 ? true : false;
 }
